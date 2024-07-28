fb1d0d2b842e49427e466d5d93d8b336f8d9df25
ryukennz <fernandordyansyah@gmail.com>
2024-07-29 00:39:59 +0700

    returning folder to where it belongs

--- server/app.js
+++ server/app.js
@@ -3,15 +3,9 @@ import dotenv from 'dotenv'
 import cors from 'cors'
 import { mongoConnect } from './config/mongoConnection.js';
 import router from './routes/index.js';
-import path from 'path'
-import { fileURLToPath } from 'url';
 
 const app = express()
 const port = process.env.PORT || 9000
-
-/** Resolving dirname for ES MODULE */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 /* CONFIG */
 dotenv.config();
@@ -21,13 +15,6 @@ app.use(express.urlencoded({ extended: true }))
 app.use(cors())
 
 app.use('/api', router)
-
-app.use(express.static(path.join(__dirname, 'client/dist')))
-
-// Render client for any path
-app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client/dist/index.html'))
-})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
